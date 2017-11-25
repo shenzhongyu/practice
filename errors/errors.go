f081e147321aa8d2ed22bf89aa31a36dbd31bbfa
sensir <975153482@qq.com>
2017-11-25 23:06:25 +0800

    errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,48 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"errors"
-	"fmt"
 )
+
+var fp = fmt.Println
 
 type argError struct {
 	arg int
 	err error
 }
 
-func (e *argError) Error () string {
+// go报错自定义， 自由定制错误格式
+func (e *argError) Error() string {
 	return "[argument:" + strconv.Itoa(e.arg) + "] --- " + e.err.Error()
 }
 
-func f1 (arg int) (int, error) {
+func f1(arg int) (int, error) {
 	if arg == 0 {
-		return arg, errors.New("can 't work with it.")
-	}
-	return arg * 2, nil
-}
-
-func f2(arg int) (int, error) {
-	if arg == 0 {
-		return arg, &argError{arg, errors.New("can 't with it.")}
+		return arg, errors.New("can't work with it.")
 	}
 	return arg * 3, nil
 }
 
-func main() {
-	for _, i := range []int {3, 4, 0} {
+func f2(arg int) (int, error) {
+	if arg == 0 {
+		return arg, &argError{arg, errors.New("can't work with it.")}
+	}
+	return arg * 4, nil
+}
+
+func main () {
+	for _, i := range []int{1, 3, 0} {
 		if r, e := f1(i); e != nil {
-			fmt.Println("f1 failed:", e)
+			fp("f1 failed:", e)
 		} else {
-			fmt.Println("f1 worked:", r)
+			fp("f1 worked:", r)
 		}
 	}
 
-	for _, j :=  range []int {2, 4, 0} {
+	for _, j := range []int{3, 8, 0} {
 		if r, e :=  f2(j); e != nil {
-			fmt.Println("f2: failed:", e)
+			fp("f2 failed:", e)
 		} else {
-			fmt.Println("f2: worked:", r)
+			fp("f2 worked:", r)
 		}
 	}
 }
